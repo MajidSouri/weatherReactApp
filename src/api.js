e87226e7d2e8b51df50755536ff5dfb2f095f415
MajidSouri <seyedmajidsouri@gmail.com>
2023-01-07 16:03:20 -0600

    React Project

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	  method: 'GET',
+	  headers: {
+		  'X-RapidAPI-Key': '4c88e6806cmsh54e008773933c3dp1323a2jsn92609a91250f',
+		  'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "dea4bd279a75a5a5328b0554934f884a";
